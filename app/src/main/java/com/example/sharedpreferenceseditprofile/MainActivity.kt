de9c6a94838ddb374288c6213ad8b72c1481a322
Erick Savluchinske <ericksav.dev@gmail.com>
2023-01-25 18:18:23 -0300

    add fun init shared preferences

--- app/src/main/java/com/example/sharedpreferenceseditprofile/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferenceseditprofile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.sharedpreferenceseditprofile
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sharedpreferenceseditprofile.databinding.ActivityMainBinding
@@ -7,10 +8,21 @@ import com.example.sharedpreferenceseditprofile.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var mySharedPreferences: SharedPreferences
+    private lateinit var myEditor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        initSharedPreferences()
+    }
+
+    private fun initSharedPreferences(){
+
+        mySharedPreferences = getSharedPreferences("my_shared_preferences", MODE_PRIVATE)
+        myEditor = mySharedPreferences.edit()
     }
 }
